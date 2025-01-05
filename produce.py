a008e5ccff0b190c5a9a7314aca196a5baffa129
QwertyJacob <jesusfcevallos@gmail.com>
2025-01-05 22:05:04 +0000

    generating from different distributions!

--- produce.py
+++ produce.py
@@ -19,6 +19,10 @@ with open('copula_anomalie.pkl', 'rb') as f:
 with open('copula_normali.pkl', 'rb') as f:
     copula_normali = pickle.load(f)
 
+# load the probabilities of the classes:
+anomaly_probabilities = pd.read_csv('generators/anomaly_cluster_probabilities.csv')
+diagnostics_probabilities = pd.read_csv('generators/diagnostics_cluster_probabilities.csv')
+
 # Constants:
 columns_to_generate = [
     'Durata', 'CabEnabled_M1', 'CabEnabled_M8', 'ERTMS_PiastraSts', 'HMI_ACPntSts_T2', 'HMI_ACPntSts_T7',
@@ -34,6 +38,14 @@ all_columns = [
     'Flotta', 'Veicolo', 'Codice', 'Nome', 'Descrizione', 'Test', 'Timestamp', 'Timestamp chiusura', 'Durata', 
     'Posizione', 'Sistema', 'Componente', 'Latitudine', 'Longitudine', 'Contemporaneo', 'Timestamp segnale'
 ] + columns_to_generate + ['Tipo_Evento', 'Tipo_Evento_Classificato']
+
+
+def parse_int_list(arg):
+    # Split the input string by commas and convert each element to int
+    try:
+        return [int(x) for x in arg.split(',')]
+    except ValueError:
+        raise argparse.ArgumentTypeError("Arguments must be integers separated by commas")
 
 
 def produce_message(data, topic_name):
@@ -52,81 +64,151 @@ def produce_message(data, topic_name):
         print(f"Error while producing message to {topic_name} : {e}")
 
 
+def sample_anomaly_from_global():
+    return copula_anomalie.sample(1)
+
+
+def sample_anomaly_from_clusters():
+    cluster_to_sample_from = np.random.choice(
+        anomaly_probabilities['cluster'],
+        size=1, 
+        p=anomaly_probabilities['probability'])
+    return anomaly_generators[cluster_to_sample_from[0]].sample(1)
+
+
 def thread_anomalie(args):
-  media_durata_anomalie = args.mu_anomalies * args.alpha
-  sigma_anomalie = 1 * args.beta
-  lognormal_anomalie = lognorm(s=sigma_anomalie, scale=np.exp(np.log(media_durata_anomalie)))
+    media_durata_anomalie = args.mu_anomalies * args.alpha
+    sigma_anomalie = 1 * args.beta
+    lognormal_anomalie = lognorm(s=sigma_anomalie, scale=np.exp(np.log(media_durata_anomalie)))
+    topic_name = f"{VEHICLE_NAME}_anomalies"
 
-  topic_name = f"{VEHICLE_NAME}_anomalies"
+    if args.anomaly_classes == list(range(0,15)):
+        sample_anomaly_function = sample_anomaly_from_global
+    else:
+        sample_anomaly_function = sample_anomaly_from_clusters
 
-  while True:
-    synthetic_anomalie = copula_anomalie.sample(1)
-    durata_anomalia = lognormal_anomalie.rvs(size=1)
-    synthetic_anomalie['Durata'] = durata_anomalia
-    synthetic_anomalie['Flotta'] = 'ETR700'
-    synthetic_anomalie['Veicolo'] = VEHICLE_NAME
-    synthetic_anomalie['Test'] = 'N'
-    synthetic_anomalie['Timestamp'] = pd.Timestamp.now()
-    synthetic_anomalie['Timestamp chiusura'] = synthetic_anomalie['Timestamp'] + pd.to_timedelta(synthetic_anomalie['Durata'], unit='s')
-    synthetic_anomalie['Posizione'] = np.nan
-    synthetic_anomalie['Sistema'] = 'VEHICLE'
-    synthetic_anomalie['Componente'] = 'VEHICLE'
-    synthetic_anomalie['Timestamp segnale'] = np.nan
+    while True:
+        synthetic_anomalie = sample_anomaly_function()
+        durata_anomalia = lognormal_anomalie.rvs(size=1)
+        synthetic_anomalie['Durata'] = durata_anomalia
+        synthetic_anomalie['Flotta'] = 'ETR700'
+        synthetic_anomalie['Veicolo'] = VEHICLE_NAME
+        synthetic_anomalie['Test'] = 'N'
+        synthetic_anomalie['Timestamp'] = pd.Timestamp.now()
+        synthetic_anomalie['Timestamp chiusura'] = synthetic_anomalie['Timestamp'] + pd.to_timedelta(synthetic_anomalie['Durata'], unit='s')
+        synthetic_anomalie['Posizione'] = np.nan
+        synthetic_anomalie['Sistema'] = 'VEHICLE'
+        synthetic_anomalie['Componente'] = 'VEHICLE'
+        synthetic_anomalie['Timestamp segnale'] = np.nan
 
-    for col in all_columns:
-        if col not in synthetic_anomalie.columns:
-            synthetic_anomalie[col] = np.nan
-    
-    synthetic_anomalie = synthetic_anomalie.round(2)
-    synthetic_anomalie = synthetic_anomalie[all_columns]
-    # print(f"Nuova anomalia generata: {synthetic_anomalie}")
-    # Convert data to JSON and send it to Kafka
-    data_to_send = synthetic_anomalie.iloc[0].to_dict()
-    data_to_send['Timestamp'] = str(data_to_send['Timestamp'])
-    data_to_send['Timestamp chiusura'] = str(data_to_send['Timestamp chiusura'])
-    produce_message(data_to_send, topic_name)
-    time.sleep(durata_anomalia[0])
+        for col in all_columns:
+            if col not in synthetic_anomalie.columns:
+                synthetic_anomalie[col] = np.nan
+
+        synthetic_anomalie = synthetic_anomalie.round(2)
+        synthetic_anomalie = synthetic_anomalie[all_columns]
+        # print(f"Nuova anomalia generata: {synthetic_anomalie}")
+        # Convert data to JSON and send it to Kafka
+        data_to_send = synthetic_anomalie.iloc[0].to_dict()
+        data_to_send['Timestamp'] = str(data_to_send['Timestamp'])
+        data_to_send['Timestamp chiusura'] = str(data_to_send['Timestamp chiusura'])
+        produce_message(data_to_send, topic_name)
+        time.sleep(durata_anomalia[0])
+
+
+def sample_normal_from_global():
+    return copula_normali.sample(1)
+
+
+def sample_normal_from_clusters():
+    cluster_to_sample_from = np.random.choice(
+        diagnostics_probabilities['cluster'],
+        size=1, 
+        p=diagnostics_probabilities['probability'])
+    return diagnostics_generators[cluster_to_sample_from[0]].sample(1)
 
 
 def thread_normali(args):
-  media_durata_normali = args.mu_normal * args.alpha
-  sigma_normali = 1 * args.beta
-  lognormal_normali = lognorm(s=sigma_normali, scale=np.exp(np.log(media_durata_normali)))
+    media_durata_normali = args.mu_normal * args.alpha
+    sigma_normali = 1 * args.beta
+    lognormal_normali = lognorm(s=sigma_normali, scale=np.exp(np.log(media_durata_normali)))
+    topic_name = f"{VEHICLE_NAME}_normal_data"
 
-  topic_name = f"{VEHICLE_NAME}_normal_data"
+    if args.diagnostics_classes == list(range(0,15)):
+        sample_normal_function = sample_normal_from_global
+    else:
+        sample_normal_function = sample_normal_from_clusters
 
-  while True:
-    synthetic_normali = copula_normali.sample(1)
-    durata_normale = lognormal_normali.rvs(size=1)
-    synthetic_normali['Durata'] = durata_normale
-    synthetic_normali['Flotta'] = 'ETR700'
-    synthetic_normali['Veicolo'] = VEHICLE_NAME
-    synthetic_normali['Test'] = 'N'
-    synthetic_normali['Timestamp'] = pd.Timestamp.now()
-    synthetic_normali['Timestamp chiusura'] = synthetic_normali['Timestamp'] + pd.to_timedelta(synthetic_normali['Durata'], unit='s')
-    synthetic_normali['Posizione'] = np.nan
-    synthetic_normali['Sistema'] = 'VEHICLE'
-    synthetic_normali['Componente'] = 'VEHICLE'
-    synthetic_normali['Timestamp segnale'] = np.nan
+    while True:
+        synthetic_normali = copula_normali.sample(1)
+        durata_normale = lognormal_normali.rvs(size=1)
+        synthetic_normali['Durata'] = durata_normale
+        synthetic_normali['Flotta'] = 'ETR700'
+        synthetic_normali['Veicolo'] = VEHICLE_NAME
+        synthetic_normali['Test'] = 'N'
+        synthetic_normali['Timestamp'] = pd.Timestamp.now()
+        synthetic_normali['Timestamp chiusura'] = synthetic_normali['Timestamp'] + pd.to_timedelta(synthetic_normali['Durata'], unit='s')
+        synthetic_normali['Posizione'] = np.nan
+        synthetic_normali['Sistema'] = 'VEHICLE'
+        synthetic_normali['Componente'] = 'VEHICLE'
+        synthetic_normali['Timestamp segnale'] = np.nan
 
-    for col in all_columns:
-        if col not in synthetic_normali.columns:
-            synthetic_normali[col] = np.nan
-    
-    synthetic_normali = synthetic_normali.round(2)
-    synthetic_normali = synthetic_normali[all_columns]
-    # print(f"Nuova diagnostica generata: {synthetic_normali}")
-    # Convert data to JSON and send it to Kafka
-    data_to_send = synthetic_normali.iloc[0].to_dict()
-    data_to_send['Timestamp'] = str(data_to_send['Timestamp'])
-    data_to_send['Timestamp chiusura'] = str(data_to_send['Timestamp chiusura'])
-    produce_message(data_to_send, topic_name)
-    time.sleep(durata_normale[0])
+        for col in all_columns:
+            if col not in synthetic_normali.columns:
+                synthetic_normali[col] = np.nan
+
+        synthetic_normali = synthetic_normali.round(2)
+        synthetic_normali = synthetic_normali[all_columns]
+        # print(f"Nuova diagnostica generata: {synthetic_normali}")
+        # Convert data to JSON and send it to Kafka
+        data_to_send = synthetic_normali.iloc[0].to_dict()
+        data_to_send['Timestamp'] = str(data_to_send['Timestamp'])
+        data_to_send['Timestamp chiusura'] = str(data_to_send['Timestamp chiusura'])
+        produce_message(data_to_send, topic_name)
+        time.sleep(durata_normale[0])
+
+
+def normalize_anomaly_probabilities(anomaly_classes):
+    global anomaly_probabilities
+    # Filter for the subset of clusters
+    anomaly_probabilities = anomaly_probabilities[anomaly_probabilities.index.isin(anomaly_classes)]
+    total_prob = anomaly_probabilities['probability'].sum()
+    # Renormalize probabilities for the subset
+    normalized_probabilities = anomaly_probabilities['probability'] / total_prob
+    anomaly_probabilities['probability'] = normalized_probabilities
+
+
+def normalize_diagnostics_probabilities(diagnostics_classes):
+    global diagnostics_probabilities
+    # Filter for the subset of clusters
+    diagnostics_probabilities = diagnostics_probabilities[diagnostics_probabilities.index.isin(diagnostics_classes)]
+    total_prob = diagnostics_probabilities['probability'].sum()
+    # Renormalize probabilities for the subset
+    normalized_probabilities = diagnostics_probabilities['probability'] / total_prob
+    diagnostics_probabilities['probability'] = normalized_probabilities
+
+
+def get_anomaly_generators_dict(anomaly_classes):
+    normalize_anomaly_probabilities(anomaly_classes)
+    anomaly_generators = {}
+    for anomaly_class in anomaly_classes:
+        with open(f'generators/anomalies/copula_anomalie_cluster_{anomaly_class}.pkl', 'rb') as f:
+            anomaly_generators[anomaly_class] = pickle.load(f)
+    return anomaly_generators
+
+
+def get_diagnostics_generators_dict(diagnostics_classes):
+    normalize_diagnostics_probabilities(diagnostics_classes)
+    diagnostics_generators = {}
+    for diagnostics_class in diagnostics_classes:
+        with open(f'generators/diagnostics/copula_normal_cluster_{diagnostics_class}.pkl', 'rb') as f:
+            diagnostics_generators[diagnostics_class] = pickle.load(f)
+    return diagnostics_generators
 
 
 def main():
     global VEHICLE_NAME, KAFKA_BROKER
-    global producer, logger
+    global producer, logger, anomaly_generators, diagnostics_generators
 
     parser = argparse.ArgumentParser(description='Kafka Producer for Synthetic Vehicle Data')
     parser.add_argument('--vehicle_name', type=str, required=True, help='Name of the vehicle')        
@@ -137,6 +219,8 @@ def main():
     parser.add_argument('--mu_normal', type=float, default=115, help='Mu parameter (mean of the mean interarrival times of normal data)')
     parser.add_argument('--alpha', type=float, default=0.2, help='Alpha parameter (scaling factor of the mean interarrival times of both anomalies and normal data)')
     parser.add_argument('--beta', type=float, default=1.9, help='Beta parameter (std dev of interarrival times of both anomalies and normal data)')
+    parser.add_argument('--anomaly_classes',  type=parse_int_list, default=list(range(0,15)))
+    parser.add_argument('--diagnostics_classes', type=parse_int_list, default=list(range(0,15)))
 
     args = parser.parse_args()
 
@@ -152,16 +236,20 @@ def main():
     }
     producer = SerializingProducer(conf_prod)
 
-    
-
-
     logger.debug(f"Setting up threads for vehicle: {VEHICLE_NAME}")
     vehicle_args=argparse.Namespace(
         mu_anomalies=args.mu_anomalies,
         mu_normal=args.mu_normal,
         alpha=args.alpha,
         beta=args.beta,
+        anomaly_classes=args.anomaly_classes,
+        diagnostics_classes=args.diagnostics_classes
     )
+
+    if args.anomaly_classes != list(range(0,15)):
+        anomaly_generators = get_anomaly_generators_dict(args.anomaly_classes)
+    if args.diagnostics_classes != list(range(0,15)):
+        diagnostics_generators = get_diagnostics_generators_dict(args.diagnostics_classes)
 
     thread1 = threading.Thread(target=thread_anomalie, args=(vehicle_args,))
     thread2 = threading.Thread(target=thread_normali, args=(vehicle_args,))
@@ -180,6 +268,7 @@ def main():
 
     thread1.join()
     thread2.join()
+
 
 if __name__ == '__main__':
     main()
